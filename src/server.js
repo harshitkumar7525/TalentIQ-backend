8c70f30ff6c6bad103bccfbe3f9bdf9fac57b0d2
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-12-17 02:36:56 +0530

    deploying on render

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 import express from "express";
-import path from "path";
 import cors from "cors";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
-import { fileURLToPath } from "url";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -14,14 +12,11 @@ import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(express.json());
 
 app.use(
   cors({
-    origin: true,        // reflects request origin
+    origin: ENV.CLIENT_URL,        // reflects request origin
     credentials: true,   // allows cookies
   })
 );
@@ -33,17 +28,9 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-app.get("/health", (_, res) => {
+app.get("/", (_, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
-
-if (ENV.NODE_ENV === "production") {
-  const frontendDist = path.join(__dirname, "../../frontend/dist");
-  app.use(express.static(frontendDist));
-  app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(frontendDist, "index.html"));
-  });
-}
 
 const startServer = async () => {
   try {
